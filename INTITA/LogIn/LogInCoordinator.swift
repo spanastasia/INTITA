8edf06e9390fc157b487c3a1731422b7c52c1f71
Stepan19897 <stepan.niemykin@gmail.com>
2020-11-02 11:07:53 +0200

    Created Log In screen with no functionality

---
+++ INTITA/LogIn/LogInCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  LogInCoordinator.swift
+//  INTITA
+//
+//  Created by Stepan Niemykin on 02.11.2020.
+//
+
+import Foundation
+import UIKit
+
+class LogInCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = LogInViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+//    func invalidScreen() {
+//        let invalidCoordinator = InvalidCoordinator(navigationController:navigationController)
+//        invalidCoordinator.start()
+//    }
+}
