f15be6a5897a8a701705191f192a7a56dde2978a
spanastasia <anastasibohdan45@gmail.com>
2020-11-02 12:05:06 +0200

    RequestApi: changed argument 'url' to 'request' in method request()

--- INTITA/Networking/Authorization.swift
+++ INTITA/Networking/Authorization.swift
@@ -7,47 +7,32 @@
 
 import Foundation
 class Authorization {
-    public static func login(email: String, password: String, completition: @escaping (Result<LoginResponse, Error>) -> Void) {
+    public static func login(email: String, password: String, completion: @escaping (Result<LoginResponse, Error>) -> Void) {
         guard let url = URL(string: RequestAPI.loginPath)
         else {
             return
         }
         let data = RequestAPI.prepareData(email: email, password: password)
-        RequestAPI.request(url: url, jsonData: data) { (result: Result<LoginResponse, Error>) in
-            completition(result)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = data
+        RequestAPI.request(request: request) { (result: Result<LoginResponse, Error>) in
+            completion(result)
         }
     }
-    public static func logout(token: String, completition: @escaping (Result<LogoutResponse, Error>) -> Void) {
+    public static func logout(token: String, completion: @escaping (Result<LogoutResponse, Error>) -> Void) {
         guard let url = URL(string: RequestAPI.logoutPath)
         else {
             return
         }
-        let sessionConfig = URLSessionConfiguration.default
-
-        let authValue: String = "Bearer \(token)"
-
-        sessionConfig.httpAdditionalHeaders = ["Authorization": authValue]
-
-        let session = URLSession(configuration: sessionConfig)
-        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        let task = session.dataTask(with: request) { data, response, error in
-            if error != nil {
-                completition(.failure(ApiError.taskError))
-                return
-            }
-            guard let data = data else {
-                completition(.failure(ApiError.noData))
-                return
-            }
-            do {
-                let response = try JSONDecoder().decode(LogoutResponse.self, from: data)
-                completition(.success(response))
-            } catch {
-                completition(.failure(error))
-            }
+        let authValue: String = "Bearer \(token)"
+        request.setValue(authValue, forHTTPHeaderField: "Authorization")
+
+        RequestAPI.request(request: request) { (result: Result<LogoutResponse, Error>) in
+            completion(result)
         }
-        task.resume()
     }
 }
