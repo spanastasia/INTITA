294e889b78f641007ed62c9ec4ad3d4897bc5fe0
Viacheslav <markvyach83@gmail.com>
2020-11-03 20:35:38 +0200

    create InvalidCoordinator.swift, InvalidViewController.swift, Invalid.storyboard

---
+++ INTITA/Invalid/InvalidCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  InvalidCoordinator.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 03.11.2020.
+//
+
+import Foundation
---
+++ INTITA/Invalid/InvalidViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InvalidViewController.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 03.11.2020.
+//
+
+import UIKit
+
+class InvalidViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
