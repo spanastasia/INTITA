31f51c9077e3496b8c7f1f4298ac096b653d4f8f
svitlana.korostelova <svitlana.korostelova@gmail.com>
2020-11-06 15:00:30 +0200

    page control refactored

--- INTITA/Modules/Welcome/CustomPageControl.swift
+++ INTITA/Modules/Welcome/CustomPageControl.swift
@@ -1,8 +1,52 @@
 //
-//  CustomPageControl.swift
+//  UIPageControlStackView.swift
 //  INTITA
 //
 //  Created by Svitlana Korostelova on 05.11.2020.
 //
 
-import Foundation
+import UIKit
+
+
+@IBDesignable class UIPageControlStackView: UIStackView { // emulates UIPageControl behaviour
+    @IBInspectable var currentPageImage: UIImage?
+    @IBInspectable var otherPagesImage: UIImage?
+    @IBInspectable var numberOfPages: Int = 0 {
+        didSet {
+            insertSubviews(numberOfPages)
+        }
+    }
+    
+    private lazy var active: UIImageView! = {
+        return subviews.first as! UIImageView
+    }()
+    
+    func transition(next: Int) { //The next active page control
+        UIView.animate(withDuration: 0.5) {
+            guard let currentDot = self.subviews[next] as? UIImageView else {return}
+            currentDot.image = self.currentPageImage
+            self.active.image = currentDot.image
+            if next != 0 {
+                guard let previousDot = self.subviews[next-1] as? UIImageView else {return}
+                
+                previousDot.image = self.otherPagesImage
+            }
+        }
+        // here should be reverce scrolling pages
+    }
+    
+    private func insertSubviews(_ n: Int) { //for the first fill
+        subviews.forEach { view in
+            view.removeFromSuperview()
+        }
+        axis = .horizontal
+        distribution = .fillEqually
+        spacing = 10
+        for _ in Array(repeating: 0, count: n) {
+            let view = UIImageView(frame: .zero)
+            view.image = otherPagesImage
+            addArrangedSubview(view)
+        }
+        active.image = currentPageImage
+    }
+}
--- INTITA/Modules/Welcome/WelcomeViewController.swift
+++ INTITA/Modules/Welcome/WelcomeViewController.swift
@@ -6,8 +6,7 @@
 //
 
 import UIKit
-// перезалить картинку лого
-// корнер радиус 10
+
 class WelcomeViewController: UIViewController, Storyboarded, UIScrollViewDelegate {
     @IBOutlet weak var mottoLabel: UILabel!
     
@@ -20,7 +19,7 @@ class WelcomeViewController: UIViewController, Storyboarded, UIScrollViewDelegat
     private var scrollView = UIScrollView(frame: .zero)
     private var stackView = UIStackView(frame: .zero)
     var views:[UIView] = []
-    private var pageControl = UIPageControl()
+    @IBOutlet weak var stackViewPageControl: UIPageControlStackView!
     
     let texts = ["promo1".localized,
                  "promo2".localized,
@@ -33,22 +32,31 @@ class WelcomeViewController: UIViewController, Storyboarded, UIScrollViewDelegat
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         startBtn.layer.cornerRadius = 10.0
         startBtn.setTitle("start".localized, for: .normal)
         startBtn.titleLabel?.adjustsFontSizeToFitWidth = true
         startBtn.titleLabel?.textAlignment = .center
         startBtn.titleLabel?.font = UIFont.primaryFontRegular
+        startBtn.titleLabel?.layer.shadowColor = UIColor.black.cgColor
+        startBtn.titleLabel?.layer.shadowOffset = CGSize(width: 0, height: 4)
+        startBtn.titleLabel?.layer.shadowOpacity = 0.5
+        startBtn.titleLabel?.layer.shadowRadius = 4.0
         
         mottoLabel.text = "moto".localized
         mottoLabel.textAlignment = .center
         mottoLabel.adjustsFontSizeToFitWidth = true
         mottoLabel.font = UIFont.primaryFontRegular
+        mottoLabel.layer.shadowColor = UIColor.black.cgColor
+        mottoLabel.layer.shadowOpacity = 0.5
+        mottoLabel.layer.shadowOffset = CGSize(width: 0, height: 4)
+        mottoLabel.layer.shadowRadius = 4.0
         
         skipBtn.setTitle("skip".localized, for: .normal)
         skipBtn.titleLabel?.adjustsFontSizeToFitWidth = true
         skipBtn.titleLabel?.font = UIFont.primaryFontThin
         skipBtn.titleLabel?.textAlignment = .left
-
+        
         logo.layer.cornerRadius = 10.0
         setupPageControll()
         setupScrollView()
@@ -60,114 +68,68 @@ class WelcomeViewController: UIViewController, Storyboarded, UIScrollViewDelegat
         coordinator?.displayLogin()
     }
     func setupScrollView() {
-      scrollView.translatesAutoresizingMaskIntoConstraints = false
-      scrollView.backgroundColor = .white
-      scrollView.showsHorizontalScrollIndicator = false
-      scrollView.isPagingEnabled = true
-      scrollView.delegate = self
-      
-      self.view.addSubview(scrollView)
-      NSLayoutConstraint.activate([
-        scrollView.topAnchor.constraint(equalTo: self.line.bottomAnchor, constant: 32),
-        scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
-        scrollView.bottomAnchor.constraint(equalTo: self.pageControl.topAnchor, constant: -24)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.delegate = self
+        
+        self.view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.line.bottomAnchor, constant: 32),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            scrollView.bottomAnchor.constraint(equalTo: self.stackViewPageControl.topAnchor, constant: -24)
         ])
     }
-
+    
     func setupStackView(scrollView: UIScrollView) {
-      stackView.translatesAutoresizingMaskIntoConstraints = false
-      stackView.distribution = .equalSpacing
-      stackView.spacing = 0
-
-      scrollView.addSubview(stackView)
-      NSLayoutConstraint.activate([
-        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0),
-        stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0),
-        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0),
-        stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0
+        
+        scrollView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16)
         ])
     }
     
     func createAndAddViews(to stackView: UIStackView) -> [UIView] {
-      var views:[UIView] = []
+        var views:[UIView] = []
         texts.forEach { (text) in
             let pageView = PromoTextView(promoText: text)
             views.append(pageView)
         }
-
-      
-      views.forEach { (view) in
-        view.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubview(view)
-        view.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor).isActive = true
-        view.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor).isActive = true
-      }
-      return views
-    }
-    
-    @objc func pageControlTapped(sender: UIPageControl) {
-      let pageWidth = scrollView.bounds.width
-      let offset = sender.currentPage * Int(pageWidth)
-      UIView.animate(withDuration: 0.33, animations: { [weak self] in
-        self?.scrollView.contentOffset.x = CGFloat(offset)
-      })
+        
+        views.forEach { (view) in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            stackView.addArrangedSubview(view)
+            view.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor).isActive = true
+            view.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor).isActive = true
+        }
+        return views
     }
     
     func setupPageControll() {
-//        pageControl.backgroundColor = .none
-//        pageControl.pageIndicatorTintColor = .black
-//        UIColor(named: "mainColor")
-        pageControl.currentPageIndicatorTintColor = UIColor(named: "mainColor")
-        pageControl.transform = CGAffineTransform(scaleX: 2, y: 2)
-//        pageControl.subviews[0].subviews[0].subviews[0].layer.borderWidth = 5
-        
-//        let dot = UIImage()
-//        dot.backgroundColor = .black
-//        dot.layer.borderColor = UIColor.red.cgColor
-//        dot.layer.borderWidth = 5
-        
-//        pageControl.subviews[0].subviews[0].insertSubview(dot, at: 0)
-//        subviews[0].layer.borderColor = UIColor.red.cgColor
-        
-        pageControl.preferredIndicatorImage = UIImage(named: "dotPageControl")
-        
-        
-        
-//        print(dots)
-//        pageControl.subviews.forEach { (view) in
-////            print(view.subviews.count)
-//            view.layer.borderWidth = 2
-//            view.layer.borderColor = UIColor.green.cgColor
-//            view.subviews[0].layer.borderWidth = 2
-//            view.subviews[0].layer.borderColor = UIColor.red.cgColor
-//            view.subviews[0].subviews[0].layer.borderWidth = 5
-//            view.subviews[0].subviews[0].layer.borderColor = UIColor.blue.cgColor
-//        }
-//        pageControl.subviews[0].subviews[0].
-        pageControl.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(pageControl)
-        pageControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        pageControl.bottomAnchor.constraint(equalTo: self.startBtn.topAnchor, constant: -50).isActive = true
-        pageControl.numberOfPages = texts.count
-            //  if switch pages will be required by user click
-//        pageControl.addTarget(self, action: #selector(pageControlTapped), for: .valueChanged)
-        
-//        pageControl.setIndicatorImage(UIImage(named: "dotPageControl"), forPage: 0)
+        stackViewPageControl.numberOfPages = texts.count
+        stackViewPageControl.transition(next: 0)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-      let pageWidth = scrollView.bounds.width
-      let pageFraction = scrollView.contentOffset.x/pageWidth
-      let constantFraction = pageFraction
-      
-      pageControl.currentPage = Int((round(pageFraction)))
-      
-      for (index, view) in views.enumerated() {
-        guard let view = view as? PromoTextView else { return }
-        let constant = pageWidth * (CGFloat(index) - constantFraction)
-        view.updateViewCenterXAnchor(with: constant)
-      }
+        let pageWidth = scrollView.bounds.width
+        let pageFraction = scrollView.contentOffset.x/pageWidth
+        let constantFraction = pageFraction
+        
+        stackViewPageControl.transition(next: Int((round(pageFraction))))
+        
+        for (index, view) in views.enumerated() {
+            guard let view = view as? PromoTextView else { return }
+            let constant = pageWidth * (CGFloat(index) - constantFraction)
+            view.updateViewCenterXAnchor(with: constant)
+        }
     }
 }
 
