2a5492ff6be7bd5fd3e72717cb9634d2d3e4939f
svitlana.korostelova <svitlana.korostelova@gmail.com>
2020-11-13 21:07:01 +0200

    protocol for indicator added

--- INTITA/Common/ActivityIndicatorView.swift
+++ INTITA/Common/ActivityIndicatorView.swift
@@ -4,19 +4,25 @@
 //
 //  Created by Svitlana Korostelova on 12.11.2020.
 //
-
 import UIKit
 
-class ActivityIndicatorView: UIViewController {
-    static var spinner = UIActivityIndicatorView(style: .large)
-    static var loadingContainerView = UIView()
+protocol ActivityIndicatorViewProtocol: class {
+    static func startSpinner()
+    static func stopSpinner()
+}
+
+
+class ActivityIndicatorView: UIViewController, ActivityIndicatorViewProtocol {
+    private static var spinner = UIActivityIndicatorView(style: .large)
+    private static var loadingContainerView: UIView?
     
-    class var root: UINavigationController {
+    class private var root: UINavigationController {
         return UIApplication.shared.windows.first!.rootViewController as! UINavigationController
     }
     
     class func startSpinner() {
         DispatchQueue.main.async {
+            loadingContainerView = UIView()
             let vc = ActivityIndicatorView()
             vc.modalPresentationStyle = .overFullScreen
             vc.view.contentMode = .scaleToFill
@@ -24,7 +30,8 @@ class ActivityIndicatorView: UIViewController {
             let blurEffect = UIBlurEffect(style: .regular)
             let blurredEffectView = UIVisualEffectView(effect: blurEffect)
             blurredEffectView.frame = vc.view.bounds
-            loadingContainerView.addSubview(blurredEffectView)
+            vc.view.addSubview(blurredEffectView)
+//            loadingContainerView?.addSubview(blurredEffectView)
             
             spinner.color = UIColor.primaryColor
             spinner.center = blurredEffectView.center
@@ -32,15 +39,22 @@ class ActivityIndicatorView: UIViewController {
             spinner.startAnimating()
             blurredEffectView.contentView.addSubview(spinner)
             
-            vc.view.addSubview(loadingContainerView)
+//            vc.view.addSubview(loadingContainerView!)
             root.present(vc, animated: true, completion: nil)
+            
         }
     }
     
     class func stopSpinner() {
         DispatchQueue.main.async {
             spinner.stopAnimating()
-            loadingContainerView.removeFromSuperview()
+//            loadingContainerView?.removeFromSuperview()
+//            root.popViewController(animated: true)
+            
+            if let currentVC = root.presentedViewController as? ActivityIndicatorView {
+                    spinner.stopAnimating()
+                    currentVC.dismiss(animated: true, completion: nil)
+                }
         }
     }
 }
