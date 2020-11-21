187d0d5345ab01b6f89b85996c2e1e271fb6ed69
spanastasia <anastasibohdan45@gmail.com>
2020-11-21 09:37:25 +0200

    add image picker

--- INTITA/Modules/Profile/ProfileViewController.swift
+++ INTITA/Modules/Profile/ProfileViewController.swift
@@ -135,5 +135,36 @@ extension ProfileViewController: ProfileViewModelDelegate {
 extension ProfileViewController: ProfileHeaderViewDataSourse {
     func editImage() {
         print("edit image")
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.isEditing = true
+        UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.modalPresentationStyle = .fullScreen
+        present(imagePickerController, animated: true)
+        
+        colorStatusBar(UIColor.systemBackground)
+    }
+}
+
+extension ProfileViewController: (UIImagePickerControllerDelegate & UINavigationControllerDelegate) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        dismiss(animated: true)
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        colorStatusBar(UIColor.primaryColor)
     }
 }
