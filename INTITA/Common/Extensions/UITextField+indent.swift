4b4a2d2c7a678bc42783d25de081bf66736a4f18
Viacheslav <markvyach83@gmail.com>
2020-12-21 15:07:26 +0200

    deleted "self" from file's UITextField+indent.swift

--- INTITA/Common/Extensions/UITextField+indent.swift
+++ INTITA/Common/Extensions/UITextField+indent.swift
@@ -9,10 +9,10 @@ import UIKit
 
 extension UITextField {
     func indent(size: CGFloat) {
-        self.leftView = UIView(frame: CGRect(x: self.frame.minX,
+        leftView = UIView(frame: CGRect(x: self.frame.minX,
                                              y: self.frame.minY,
                                              width: size,
                                              height: self.frame.height))
-        self.leftViewMode = .always
+        leftViewMode = .always
     }
 }
