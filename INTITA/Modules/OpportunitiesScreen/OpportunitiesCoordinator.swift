acf509d2e85fbf1809c2c96a64988e7b1159f5f0
Viacheslav <markvyach83@gmail.com>
2021-01-24 18:08:00 +0200

    created Opportunities.storyboard, OpportunitiesCoordinator.swift, OpportunitiesViewModel.swift, OpportunitiesViewController.swift

---
+++ INTITA/Modules/OpportunitiesScreen/OpportunitiesCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  OpportunitiesCoordinator.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 24.01.2021.
+//
+
+import Foundation
---
+++ INTITA/Modules/OpportunitiesScreen/OpportunitiesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OpportunitiesViewModel.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 24.01.2021.
+//
+
+import Foundation
