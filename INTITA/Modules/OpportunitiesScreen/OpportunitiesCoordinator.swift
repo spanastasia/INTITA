18df51e61f1941499e97e20cf58bc93e5d105187
Viacheslav <markvyach83@gmail.com>
2021-01-24 18:26:57 +0200

    created Push to Opportunities Screen

--- INTITA/Modules/OpportunitiesScreen/OpportunitiesCoordinator.swift
+++ INTITA/Modules/OpportunitiesScreen/OpportunitiesCoordinator.swift
@@ -5,4 +5,22 @@
 //  Created by Viacheslav Markov on 24.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class OpportunitiesCoordinator: Coordinator {
+    
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        
+        let vc = OpportunitiesViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+}
