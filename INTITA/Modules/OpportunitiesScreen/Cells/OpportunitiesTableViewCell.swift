7540b73baea7c88d3b0e7a9edeab8fd669efb3e7
Viacheslav <markvyach83@gmail.com>
2021-01-24 19:31:03 +0200

    created cell of opportunities

---
+++ INTITA/Modules/OpportunitiesScreen/Cells/OpportunitiesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OpportunitiesTableViewCell.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 24.01.2021.
+//
+
+import UIKit
+
+class OpportunitiesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
