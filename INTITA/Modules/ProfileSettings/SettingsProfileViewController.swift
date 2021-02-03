76c988f33d45e679a36f2364373639ba6b4c0577
Viacheslav <markvyach83@gmail.com>
2021-02-03 20:29:57 +0200

    created file SettingsProfileViewModel.swift

--- INTITA/Modules/ProfileSettings/SettingsProfileViewController.swift
+++ INTITA/Modules/ProfileSettings/SettingsProfileViewController.swift
@@ -12,15 +12,7 @@ class SettingsProfileViewController: UIViewController, Storyboarded {
     var coordinator: SettingsProfileCoordinator?
     private var isProfileEditing = false
 
-    let arr = [
-                "firstName", "full_name", "secondName",
-                "nickname", "birthday", "email",
-                "facebook", "linkedin", "twitter",
-                "phone", "address", "education",
-                "aboutUs", "aboutMy", "avatar",
-                "skype", "country", "city",
-                "current_job", "userStatus"
-            ]
+    var viewModel = SettingsProfileViewModel()
 
     @IBOutlet weak var tableView: UITableView!
 
@@ -68,7 +60,7 @@ extension SettingsProfileViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return arr.count
+        return viewModel.numberOfStates
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -94,7 +86,7 @@ extension SettingsProfileViewController: UITableViewDataSource {
         } else {
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "InfoSettingProfileTableViewCell", for: indexPath) as! InfoSettingProfileTableViewCell
-            cell.aboutSelfLabel.text = arr[indexPath.row] + " : "
+            cell.aboutSelfLabel.text = viewModel.arrayItems[indexPath.row] + " : "
             cell.isProfileEditing = isProfileEditing
             if indexPath.row.isMultiple(of: 2) {
                 cell.backgroundColor = .systemGray6
