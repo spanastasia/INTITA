3053533958592011683450e23be248b4590e9049
Viacheslav <markvyach83@gmail.com>
2021-02-06 08:22:29 +0200

    added file OpportunitiesModel to folder models

---
+++ INTITA/Models/OpportunitiesModel.swift
@@ -0,0 +1,71 @@
+//
+//  OpportunitiesModel.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 25.01.2021.
+//
+
+import Foundation
+
+enum Opportunities: Int, Codable, CaseIterable {
+    case course
+    case task
+    case study
+    
+    var sectionTitle: String {
+        switch self {
+ 
+        case .course:
+            return "courses_modules_finance".localized
+        case .task:
+            return "tasks_detailed_answer".localized
+        case .study:
+            return "type_training".localized
+        }
+    }
+    
+    var sectionInfo: String {
+        switch self {
+ 
+        case .course:
+            return "information_about_course".localized
+        case .task:
+            return "answer_assessments".localized
+        case .study:
+            return "courses_modules_etc".localized
+        }
+    }
+    
+    var sectionButonTask: String {
+        switch self {
+ 
+        case .course:
+            return "finances".localized
+        case .task:
+            return "tasks_detailed_answer".localized
+        case .study:
+            return "type_training".localized
+        }
+    }
+    
+    var sectionButonFinance: String {
+        switch self {
+ 
+        case .course:
+            return "available_courses_modules".localized
+        default: return ""
+        }
+    }
+    
+    var itemsCount: Int {
+        switch self {
+
+        case .course:
+            return 2
+        case .task:
+            return 1
+        case .study:
+            return 1
+        }
+    }
+}
