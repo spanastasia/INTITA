08037215d5ced8a967a6575658527d60e45c7129
Svitlana Korostelova <svitlana.korostelova@gmail.com>
2021-02-12 14:34:48 +0200

    The colours of priorities were added. Low priority was added. JSON Id as rawValue for priorities was added.

--- INTITA/Common/Extensions/UIColor+Additions.swift
+++ INTITA/Common/Extensions/UIColor+Additions.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 extension UIColor {
-  class var primaryColor: UIColor {
-    return UIColor(red: 75.0 / 255.0, green: 117.0 / 255.0, blue: 164.0 / 255.0, alpha: 1.0)
-  }
+    class var primaryColor: UIColor {
+        return UIColor(red: 75.0 / 255.0, green: 117.0 / 255.0, blue: 164.0 / 255.0, alpha: 1.0)
+    }
+    class var mediumPriorityColor: UIColor {
+        return UIColor(red: 255/255, green: 165/255, blue: 0/255, alpha: 1.0)
+    }
+    class var highPriorityColor: UIColor {
+        return UIColor(red: 218/255, green: 77/255, blue: 68/255, alpha: 1.0)
+    }
+    class var urgentPriorityColor: UIColor {
+        return UIColor(red: 120/255, green: 8/255, blue: 35/255, alpha: 1.0)
+    }
 }
