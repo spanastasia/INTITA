7ddaab0202d4202cb1901c1c907d411aedc7112e
Viacheslav <markvyach83@gmail.com>
2021-03-11 17:26:45 +0200

    created Careers.json and CareerModel.swift files

---
+++ INTITA/JSONFiles/CareerModel.swift
@@ -0,0 +1,25 @@
+//
+//  CareerModel.swift
+//  INTITA
+//
+//  Created by Viacheslav Markov on 11.03.2021.
+//
+
+import Foundation
+
+struct CareerModel: LocalizedResponseProtocol {
+    
+    var id: Int
+    var titleUA: String
+    var titleRU: String
+    var titleEN: String
+    
+    static var type: LocationType { .specialization }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case titleUA = "title_ua"
+        case titleRU = "title_ru"
+        case titleEN = "title_en"
+    }
+}
