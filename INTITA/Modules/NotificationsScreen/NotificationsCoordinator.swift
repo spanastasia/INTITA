ead52e80b77f765d511d800a877e09cd3de4bf4f
Manilko <Manilko@1503gmail.com>
2021-04-22 22:23:26 +0300

    creating files to NotificationsScreen

---
+++ INTITA/Modules/NotificationsScreen/NotificationsCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationsCoordinator.swift
+//  INTITA
+//
+//  Created by Yevhenii Manilko on 22.04.2021.
+//
+
+import UIKit
+
+class NotificationsCoordinator: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
