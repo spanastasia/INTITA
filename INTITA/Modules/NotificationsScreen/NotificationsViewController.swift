b6995d9f6f28c535fef61616d7df2505c855d138
Manilko <Manilko@1503gmail.com>
2021-04-22 22:32:44 +0300

    creating object in Storibord

--- INTITA/Modules/NotificationsScreen/NotificationsViewController.swift
+++ INTITA/Modules/NotificationsScreen/NotificationsViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class NotificationsViewController: UIViewController {
 
+    @IBOutlet weak var nameNotificationsLabel: UILabel!
+    @IBOutlet weak var notificationTabelView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonToProfile(_ sender: Any) {
+        
     }
-    */
+    
 
 }
